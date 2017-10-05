e7cb544d8cf1d703920638dea762f6f91b50090b
Alkaid Darooga <DCoseT@gmail.com>
2017-10-05 08:59:05 -0500

    Añadimos Statements de C++

--- CPP-CodeExamples/CPPCodeExamples.cpp
+++ CPP-CodeExamples/CPPCodeExamples.cpp
@@ -1,14 +1,35 @@
 // CPPCodeExamples.cpp : Defines the entry point for the console application.
 //
 
+//Siempre tenemos que iniciar con la libreria stdafx.h incluida para programas de C++
 #include "stdafx.h"
 #include <iostream>
 
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+
 	
     return 0;
+}
+
+int helloworld()
+{
+	//Utilizamos cout para mostrar mensaje Hello World
+	//Utilizamos GetChar para que la consola no se cierre
+	// podemos usar tambien estos metodos
+	//std::cin.clear(); // reset any error flags
+	//std::cin.ignore(32767, '\n'); // ignore any characters in the input buffer until we find an enter character
+	//std::cin.get(); // get one more char from the user
+	std::cout << "Hello World" << std::endl;
+	std::getchar();
+	return 0;
+}
+
+int statements()
+{
+	const int x = 5;
+	std::cout << x;
+	return 0;
 }
 
