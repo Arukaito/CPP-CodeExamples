84439b0657e8bd1df575fb9bf2a412c0ca3be257
Alkaid Darooga <DCoseT@gmail.com>
2017-10-05 11:41:09 -0500

    Iniciamos con los ejemplos de las variables

--- CPP-CodeExamples/CPPCodeExamples.cpp
+++ CPP-CodeExamples/CPPCodeExamples.cpp
@@ -28,8 +28,20 @@ int helloworld()
 
 int statements()
 {
+	//Declaramos la variable
 	const int x = 5;
+	//imprimimos la variable x
 	std::cout << x;
 	return 0;
+
+	int y;      // definimos la variable como un INT
+	y = 4;      // 4 evalua a 4 que luego asigna Y
+	y = 2 + 5;  // 5 + 2 evalua a 7 que luego asigna a Y
+
+	int z;      // define el INT de Z
+	z = y;      // y evalua a 7 en el ejemplo anterior que evalua a Z
+	z = z;      // z evalua a 7 quien evalua a 7 ( INSERVIBLE )
+	z = z + 1;  // Z + 1 evalua A 8, que luego es asignado a z.
+
 }
 
